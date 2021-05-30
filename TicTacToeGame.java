810e5cf0b908a75628bcc1e705c2f8d44df5ddbe
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-05-30 21:42:44 +0530

    Shown Board Method and done show Correction

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -10,7 +10,7 @@ public class TicTacToeGame {
 	static String player1 = "";
 	static String player2 = "";
 
-	public static void main(String args[]) {
+	public static void main(String args[]) throws IOException {
 		gameBoard = new char[10];
 		gameBoard[0] = ' ';
 		gameBoard[1] = ' ';
@@ -23,9 +23,12 @@ public class TicTacToeGame {
 		gameBoard[8] = ' ';
 		gameBoard[9] = ' ';
 		selectXandZero();
+		if(player1 != "" && player2 != "") {
+			showBoard();
+		}
 	}
 
-	public static void selectXandZero() {
+	public static void selectXandZero() throws IOException {
 		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("Please choose X or O");
 		try {
@@ -33,14 +36,30 @@ public class TicTacToeGame {
 			if (playerSelected.toUpperCase().equals("X")) {
 				player1 = playerSelected.toUpperCase();
 				player2 = "O";
+				diplayPlayerSelectedChoose();
 			} else if (playerSelected.toUpperCase().equals("O")) {
 				player1 = playerSelected;
 				player2 = "X";
+				diplayPlayerSelectedChoose();
 			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+			throw new IOException("Please select valid choose input");
 		}
+	}
 
+	static void diplayPlayerSelectedChoose() {
+		System.out.println("User Player: " + player1 + " Computer: " +player2);
+	}
+	
+	static void showBoard() {
+		System.out.println("|---|---|---|");
+		System.out.println("| " + gameBoard[1] + " | " + gameBoard[2] + " | " + gameBoard[3] + " |");
+		System.out.println("|-----------|");
+		System.out.println("| " + gameBoard[4] + " | " + gameBoard[5] + " | " + gameBoard[6] + " |");
+		System.out.println("|-----------|");
+		System.out.println("| " + gameBoard[7] + " | " + gameBoard[8] + " | " + gameBoard[9] + " |");
+		System.out.println("|---|---|---|");
 	}
 }
