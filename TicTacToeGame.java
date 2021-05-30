079ca43bc6df960f993034f71d494ac5958aeb8a
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-05-30 21:26:24 +0530

    Added method to choose X and O

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,7 +1,14 @@
 package TicTaeToc;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class TicTacToeGame {
 	static char[] gameBoard;
+	static String playerSelected;
+	static String player1 = "";
+	static String player2 = "";
 
 	public static void main(String args[]) {
 		gameBoard = new char[10];
@@ -15,5 +22,25 @@ public class TicTacToeGame {
 		gameBoard[7] = ' ';
 		gameBoard[8] = ' ';
 		gameBoard[9] = ' ';
+		selectXandZero();
+	}
+
+	public static void selectXandZero() {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		System.out.println("Please choose X or O");
+		try {
+			playerSelected = reader.readLine();
+			if (playerSelected.toUpperCase().equals("X")) {
+				player1 = playerSelected.toUpperCase();
+				player2 = "O";
+			} else if (playerSelected.toUpperCase().equals("O")) {
+				player1 = playerSelected;
+				player2 = "X";
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 }
