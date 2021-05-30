737243327db9e06ae0c8fa46f97a3b6f2ea09b33
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-05-30 21:11:40 +0530

    Created the TicTaeToc Main Class

---
+++ TicTacToeGame.java
@@ -0,0 +1,19 @@
+package TicTaeToc;
+
+public class TicTacToeGame {
+	static char[] gameBoard;
+
+	public static void main(String args[]) {
+		gameBoard = new char[10];
+		gameBoard[0] = ' ';
+		gameBoard[1] = ' ';
+		gameBoard[2] = ' ';
+		gameBoard[3] = ' ';
+		gameBoard[4] = ' ';
+		gameBoard[5] = ' ';
+		gameBoard[6] = ' ';
+		gameBoard[7] = ' ';
+		gameBoard[8] = ' ';
+		gameBoard[9] = ' ';
+	}
+}
