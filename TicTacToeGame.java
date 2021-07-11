4afb7a130c6f4635d6d85942b74c6332679cd226
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-07-11 09:48:28 +0530

    [Abhishek] Create the Board

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,6 +1,32 @@
 package TicTaeToc;
 
 public class TicTacToeGame {
-	public static void main(String args[])  {
+	static char[] board = new char[10];
+	
+	static void Board() {
+		
+		System.out.println(board[1] + "  | " + board[2] + " | " + board[3]);
+        System.out.println("-----------");
+        System.out.println(board[4] + "  | " + board[5] + " | " + board[6]);
+        System.out.println("-----------");
+        System.out.println(board[7] + "  | " + board[8] + " | " + board[9]);
 	}
+	
+	public static void main(String args[])  {
+		board[0] = '\0';
+		board[1] = '\0';
+		board[2] = '\0';
+		board[3] = '\0';
+		board[4] = '\0';
+		board[5] = '\0';
+		board[6] = '\0';
+		board[7] = '\0';
+		board[8] = '\0';
+		board[9] = '\0';
+		
+		Board();
+	}		 
+		
+	
+	
 }
