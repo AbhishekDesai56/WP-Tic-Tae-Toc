3d88a0201fde69eaaf31675b653920ad09103457
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-07-11 10:28:21 +0530

    [Abhishek] Add method to allow player to enter valid input

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -1,8 +1,15 @@
 package TicTaeToc;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class TicTacToeGame {
 	static char[] board = new char[10];
+	static String  playerSelected;
+	static char player1, player2;
 	
+	static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 	static void Board() {
 		
 		System.out.println(board[1] + "  | " + board[2] + " | " + board[3]);
@@ -10,6 +17,24 @@ public class TicTacToeGame {
         System.out.println(board[4] + "  | " + board[5] + " | " + board[6]);
         System.out.println("-----------");
         System.out.println(board[7] + "  | " + board[8] + " | " + board[9]);
+	}
+	
+    static void selectXandZero() {
+		System.out.println("Please choose X or O");
+		try {
+			playerSelected = reader.readLine();
+			if (playerSelected.toUpperCase().equals("X")) {
+				player1 = 'X';
+				player2 = 'O';
+			} else if (playerSelected.toUpperCase().equals("O")) {
+				player1 = 'O';
+				player2 = 'X';
+			}
+			System.out.println("Player 1: " + player1 + " Player 2: " + player2);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 	
 	public static void main(String args[])  {
@@ -25,6 +50,7 @@ public class TicTacToeGame {
 		board[9] = '\0';
 		
 		Board();
+		selectXandZero();
 	}		 
 		
 	
